0bc6c55a537021d3c6a6cbf41f4c648071dec8a8
Fabian Salamo <fabian278@gmail.com>
2024-02-23 13:33:24 -0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 # !/bin/bash	
 
 curl https://raw.githubusercontent.com/fabiansalamo/the-littlest-jupyterhub-docker/main/bootstrap/bootstrap.py \
-| sudo python3 - --admin ${1}
+| sudo python3 - --admin ${1} - \
   --user-requirements-txt-url https://raw.githubusercontent.com/fabiansalamo/the-littlest-jupyterhub-docker/main/bootstrap/requirements.txt
