e694af6d0d451050b943845480219e0d2aa1b93a
Fabian Salamo <fabian278@gmail.com>
2024-02-23 11:41:41 -0300

    Updated install.sh with new requirements.txt location

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@
 
 curl https://raw.githubusercontent.com/fabiansalamo/the-littlest-jupyterhub-docker/main/bootstrap/bootstrap.py \
 | sudo python3 - --admin ${1}
-  --user-requirements-txt-url https://raw.githubusercontent.com/fabiansalamo/the-littlest-jupyterhub-docker/main/requirements.txt
+  --user-requirements-txt-url https://raw.githubusercontent.com/fabiansalamo/the-littlest-jupyterhub-docker/main/bootstrap/requirements.txt
